1a122d06be886087bc9cb33f4115a7257b23026f
JiaXingBingGan <729869585@qq.com>
2016-09-20 12:50:04 +0800

    ff

---
+++ Model/StudentGroupModel.class.php
@@ -0,0 +1,7 @@
+<?
+namespace Model;
+use Think\Model;
+
+class StudentGroupModel extends Model{
+
+}
--- Student/Controller/StuController.class.php
+++ Student/Controller/StuController.class.php
@@ -26,6 +26,44 @@ class StuController extends Controller{
 		$this->display();
 	}
 	function team_manage(){
-		$this->display();
+	       $projectTeam_sql = "select A.course_name,A.course_id,B.project_name,B.project_status,D.group_id,D.group_manage from course as A,project as B,student_group as D where A.course_id=B.course_id and B.project_id=D.project_id ;";
+	 	   $course = new \Model\CourseModel();
+	 	   $team =  $course->query($projectTeam_sql);
+	 	   foreach($team as $key => $s){
+			   if($team[$key]['project_status'] == 0){
+					$team[$key]['project_status'] = '<p style="color: red;">拒绝</p>';
+					$team[$key]['group_manage'] = ' <button type="button" class="btn btn-danger">删除</button>';
+			   }
+				else if($team[$key]['project_status'] == 1){
+                    $team[$key]['project_status'] = '<p style="color: blue;">待审核</p>';
+                    $team[$key]['group_manage'] = '  <button class="btn btn-info" data-toggle="modal" data-target="#myModal">管理</button><br>
+                                                     <button type="button" class="btn btn-danger">删除</button>';
+                }
+			   else{
+					 $team[$key]['project_status'] = '<p style="color: green;">已通过</p>';
+					 $team[$key]['group_manage'] = ' <button class="btn btn-info" data-toggle="modal" data-target="#myModal">管理</button>';
+			   }
+	 	   }
+	 	   $this->assign('team',$team);
+	 	   $groupMember_sql = "select C.student_id,C.student_name from course as A,project as B,student as C,student_group as D where A.course_id=B.course_id and B.project_id=D.project_id and D.group_id=C.group_id; ";
+   		   $studentid = $course->query($groupMember_sql);
+   		   $this->assign('studentid',$studentid);
+
+
+   		    if(!empty($_GET)){
+				dump($_GET['student_id']);
+            }else{
+
+            }
+ 			//$msg=M('student');
+           // $result = $msg->where('student_id = 2')->delete();
+            //dump($result);
+
+   		   if(!empty($_POST)){
+   		   		dump($_POST['student_id']);
+   		   }else{
+				$this->display();
+   		   }
+
 	}
 }
