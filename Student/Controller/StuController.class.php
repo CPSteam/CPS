be6d9857d591d051fbf232c554e279afe3a66e4d
JiaXingBingGan <729869585@qq.com>
2016-09-25 22:24:22 +0800

    数据调用

--- Student/Controller/StuController.class.php
+++ Student/Controller/StuController.class.php
@@ -39,7 +39,7 @@ class StuController extends Controller{
 		$report_sql = "select A.course_name,A.course_id,B.project_name,B.project_id,B.teacher_id,B.project_status,B.main_project,B.middle_expected_result,B.final_expected_result,B.project_comment_info from course as A,project as B where A.course_id=B.course_id and B.project_id=".$project_id;
 		 $course = new \Model\CourseModel();
 		 $report = $course -> query($report_sql);
-		 dump($report);
+		 
 		 foreach($report as $key => $s){
 			   if($report[$key]['project_status'] == 0){
 					$report[$key]['project_status'] = '<p style="color: red;">拒绝</p>';
