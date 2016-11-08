d58bd0b7aa8c7bf9d09eafe10272277cd4ce4f8c
wulichenyang <943934049@qq.com>
2016-11-08 21:47:53 +0800

    add status

--- Teacher/Controller/ProfessorController.class.php
+++ Teacher/Controller/ProfessorController.class.php
@@ -27,6 +27,17 @@ class ProfessorController extends Controller {
 		$course_id = I('course_id');
 		$info = M("Project") -> table('project as A,teacher as B') -> where("A.course_id = '$course_id' and A.teacher_id = B.teacher_id") -> field('A.project_id, A.teacher_id, A.project_name, A.project_status, A.main_project, B.teacher_name') -> select();
 		$this -> assign('login_url',U('Home/Login/login'));
+
+		foreach($info as $key => $s){
+			   if($info[$key]['project_status'] == 0) {
+					 $info[$key]['project_status'] = '<p style="color: red;">拒绝</p>';
+			   } else if($info[$key]['project_status'] == 1) {
+              $info[$key]['project_status'] = '<p style="color: blue;">待审核</p>';
+           } else {
+					 			$info[$key]['project_status'] = '<p style="color: green;">已通过</p>';
+			       }
+		}
+
 		$this -> assign('info',$info);
 		$this->display();
 	}
